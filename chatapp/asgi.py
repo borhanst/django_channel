0c6417ca27a7745e8bc786cf369b0666453af8eb
mdborhanst <mdborhan.st@gmail.com>
2024-01-02 19:23:20 +0600

    add env file

--- chatapp/asgi.py
+++ chatapp/asgi.py
@@ -13,6 +13,10 @@ from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from chat.routers import websocket_urlpatterns
+import dotenv
+from chatapp.settings import BASE_DIR
+
+dotenv.read_dotenv(BASE_DIR/ '.env')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
 
