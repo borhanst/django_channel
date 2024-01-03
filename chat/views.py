817c75650800da51a5d28802b1f2a10388b5fab6
mdborhanst <mdborhan.st@gmail.com>
2024-01-03 17:58:58 +0600

    create chat room and group room view

--- chat/views.py
+++ chat/views.py
@@ -1,45 +1,91 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.contrib.auth import authenticate, login
-
+import uuid
+from chat.models import Room, Message
 # Create your views here.
 
 
-def home(request):
-    # message = Message.objects.filter(room='mnA32h').filter(Q(sender=1) | Q(sender=2)).values("group_name", "room").distinct()
-    # print(message)
-    return render(request, "home.html")
+def home(request, pk):
+    groups = Room.objects.filter(store=1, is_group=True, message__sender=pk)
+    rooms = (
+        Room.objects.filter(store=2, is_group=False)
+        .values("message__sender", "room_id")
+        .distinct("message__sender")
+    )
+    room_list = (
+        Room.objects.filter(store=2, is_group=False)
+        .values_list("message__sender", flat=True)
+        .distinct("message__sender")
+    )
+    context = {"groups": groups, "rooms": rooms, "pk": pk, 'room_list': room_list}
+    return render(request, "home.html", context)
 
 
-def room(request, room_id=None):
-    # rev_id = request.GET.get('id')
-    # chat_type = request.GET.get('type')
-    # group_name = request.GET.get('name')
-    # if room_id is None:
-    #     room_id = str(uuid.uuid4())[0:6]
-    # chat, created = Chat.objects.get_or_create(chat_id=room_id)
+def room(request, pk, room_id=None):
+    
 
-    # if created and not chat_type:
-    #     chat.admin = request.user
-    #     chat.customer = User.objects.get(id=rev_id)
-    #     chat.save()
-    # if chat_type:
-    #     chat_type.chat_type = chat_type
-    #     chat.members.add(request.user)
-    #     chat.group = group_name
-    # print(chat)
-    # message = Message.objects.filter(room='mnA32h')
-    return render(request, "room.html")
+    if room_id is None:
+        room_id = uuid.uuid4()
+        room = Room.objects.create(room_id=room_id)
 
+        room.is_group = False
+        room.store = 2
+        
+        room.save()
+        user = 2 if pk == 1 else 1
+        
+        message_obj = [
+            Message(
+                sender=pk,
+                event_type="room_create",
+                body=f"start chat with {user}",
+                room=room,
+            ),
+            Message(
+                sender=user,
+                event_type="add_room",
+                body=f"start chat with {pk}",
+                room=room,
+            ),
+        ]
+
+        messages = Message.objects.bulk_create(message_obj)
+    else:
+        messages = Message.objects.filter(room__room_id=room_id)
+    context = {"messages": messages, "pk": pk, "room_name": room_id}
+    return render(request, "room.html", context)
+
+def group_create(request, pk):
+    group_name = request.GET.get('name')
+    room_id = uuid.uuid4()
+    room = Room.objects.create(room_id=room_id)
+
+    
+    room.is_group = True
+    room.group_display_name=group_name
+    room.store = 1
+    
+    
+    room.save()
+    msg = Message(
+        sender=pk,
+        event_type="group_create",
+        body=f"{pk} create this group",
+        room=room,
+    )
+    msg.save()
+    return redirect(reverse_lazy('chat_room', kwargs={'room_id': room.room_id, 'pk': pk}))
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        username = request.POST.get("username")
+        password = request.POST.get("password")
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('/')
+            return redirect("/")
         else:
-            print('login error', user)
-    
-    return render(request, 'login.html')
+            print("login error", user)
+
+    return render(request, "login.html")
